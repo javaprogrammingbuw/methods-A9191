b8124ee441430a2211b6b1b525e3d5bf8f28fa92
Afshin Ghasemi <afshin.ghasemi@uni-weimar.de>
2019-03-20 10:55:41 +0100

    local changes to project

--- Circle.java
+++ Circle.java
@@ -1,5 +1,31 @@
+package methods;
+import java.util.Scanner;
+
 
 public class Circle {
+	public static void main(String[]args){
+		Scanner input = new Scanner(System.in);
+
+		
+		double PI = 3.14;
+		double radius = 0;
+		double diameter;
+		double area;
+		double circumference;
+		 System.out.print("Enter radius value:");
+
+        radius=input.nextInt();
+
+		diameter=(radius*2);
+		area= (radius*radius*PI);
+		circumference=(radius*2*PI);
+
+		    System.out.println("area is" + area);
+	        System.out.println("diameter is "+ diameter);
+	        System.out.println("circumference is "+ circumference);
+	        
+	    } 
+
 	//todo: add some methods to compute a circle's diameter, circumference and area.
 	//print some outputs to confirm you methods are working right.
 }
--- Prime.java
+++ Prime.java
@@ -1,7 +1,109 @@
+package method;
+
 import java.util.Scanner;
 
-
+/*
 public class Prime {
+	
+	
+	int inputnum=0;
+	
+	
+	public static void main(String[]args){
+		Scanner input = new Scanner(System.in);
+			
+		
+		System.out.print( "Please enter a  number: " );
+		int num = input.nextInt();
+		if( isprime( num ) ) {
+			System.out.println( "Nice - the number is prime!" );
+		} else {
+			System.out.println( "Ooops - the number is not prime!" );
+		}
+
+	}
+	
+	public static boolean isprime( int inputnum ) {
+				if((inputnum != 5)|( inputnum%2 == 0 | inputnum%3 == 0 | inputnum%5 == 0 ) | (inputnum == 0 ) | (inputnum == 1) | (inputnum != 2) | (inputnum != 3)  )
+		{
+			return false;
+		}
+	
+		else		{
+			return true;
+		}
+	}
+
+}
+
+*/
+
+public class Prime 
+{
+
+public static void main(String[] args) 
+ {
+    Scanner in = new Scanner(System.in);
+    int number, i, a = 0;
+
+    System.out.println("Enter a prime number: ");
+    number = in.nextInt();
+
+    System.out.println(isprime(number));
+ }
+
+
+
+
+public static boolean isprime(int a){
+	
+	if(a==0){
+		return true;
+	}
+	else if(a<0){
+		System.out.println("Wrong range");
+		return false;
+	}
+	
+    
+	for (int i = 2; i < a; i++)
+    {
+        if (a % i == 0)
+        {
+            return false;
+        }
+    }
+	return true;
+    }
+
+
+public int findDoublette(int j){
+	boolean b = isprime(j);
+	int Max = Integer.MAX_VALUE;
+	if (b=true) {
+	    System.out.println("Enterd number is not a prime number: ");
+	}
+	else {
+		for( j = 2; j<Max; j++){  
+			int x = 0;
+			for(int i=2; i<=j/2; i++){  
+
+				if(j%i==0){
+					x++;
+				}                      
+			}
+			if(x==0){
+
+	
+}
+}
+
+	}
+	return j;
+
+	}
+	
+	}	
 		
 	//todo: write a method isPrime which tells you if a given number is a prime or not
 	//then, write another method findDoublette which takes an integer value as input and 
@@ -11,5 +113,4 @@ public class Prime {
 	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
 	//the function should return 0.
 		
-	
-}
+
