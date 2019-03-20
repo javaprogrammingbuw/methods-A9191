4b5c11cd8f43f94ac599bcf1a8fddcccc02f49b2
Afshin Ghasemi <yepu7258@in.uni-weimar.de>
2019-03-20 10:40:13 +0100

    Complete

--- Prime.java
+++ Prime.java
@@ -1,7 +1,57 @@
+package methods;
+
 import java.util.Scanner;
 
+public class Prime 
+{
 
-public class Prime {
+public static void main(String[] args) 
+ {
+    Scanner in = new Scanner(System.in);
+    int number, i, a = 0;
+
+    System.out.print("Enter a number: ");
+    number = in.nextInt();
+
+    System.out.println("is prime -> "+isprime(number));
+    System.out.println("first number of prime doublette -> "+findDoublette(number));
+    
+ }
+
+	public static boolean isprime(int a){	
+		if(a==0){
+			return true;
+		}
+		else if(a<0){
+			System.out.println("Wrong range");
+			return false;
+		}  
+		for (int i = 2; i < a; i++)
+	    {
+	        if (a % i == 0)
+	        {
+	            return false;
+	        }
+	    }
+		return true;
+    }
+
+	public static int findDoublette(int doublette) {
+		// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+		//   x x   x   x        x     x     
+		int max = Integer.MAX_VALUE;
+		int res = -1;
+		for (int i = doublette; i < max ; i++)
+		{
+			if( isprime(i) && isprime(i+2) ) {		 
+				res = i;
+				break;
+			}			
+		}
+		
+		return res;
+	}
+}
 		
 	//todo: write a method isPrime which tells you if a given number is a prime or not
 	//then, write another method findDoublette which takes an integer value as input and 
@@ -11,5 +61,4 @@ public class Prime {
 	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
 	//the function should return 0.
 		
-	
-}
+
