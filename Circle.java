e83c068ca86cbf8089e529208be342b4dda5df3b
afshin.Ghasemi@uni-weimar.de <yepu7258@in.uni-weimar.de>
2019-03-26 12:17:31 +0100

    <final>

--- Circle.java
+++ Circle.java
@@ -9,8 +9,7 @@ public class Circle {
 	static double circumference;
 		
 	
-	public static void main(String[]args){
-		
+	public static void main(String[]args){		
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter radius value:");
         radius=input.nextInt();
@@ -19,22 +18,19 @@ public class Circle {
         circumference(radius);      
 	}
 
-		public static void diameter(double radius){
-       
+	public static void diameter(double radius){
 		diameter=(radius*2);		
-        System.out.println("diameter is "+ diameter);
+        	System.out.println("diameter is "+ diameter);
         } 
         
         public static void area(double radius){
-
-		area= (radius*radius*PI);	
-	    System.out.println("area is" + area);
+ 	        area= (radius*radius*PI);	
+	        System.out.println("area is" + area);
         }
         
-        public static void circumference(double radius){
-            
+        public static void circumference(double radius){        
 		circumference=(radius*2*PI);
-        System.out.println("circumference is "+ circumference);
+        	System.out.println("circumference is "+ circumference);
         }
 	//todo: add some methods to compute a circle's diameter, circumference and area.
 	//print some outputs to confirm you methods are working right.
